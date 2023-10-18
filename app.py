601c5cfec4a989dd9e7dd14ee30c1c2f0650be52
Makorg123 <132anaskhan@gmail.com>
2023-10-18 23:39:05 +0530

    added

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+
+# Load the dataset and perform any necessary preprocessing
+# df = pd.read_csv('data.csv')
+# ...
+
+# Define the functions for each page
+def home():
+   st.title('Home Page')
+   st.image('img1.jpeg', use_column_width=True)
+   # ...
+
+def eda():
+   st.title('EDA Page')
+   # ...
+
+def prediction():
+   st.title('Prediction Page')
+   # ...
+
+# Use streamlit to create a multi-page app with a navigation sidebar
+st.sidebar.title('Navigation')
+options = ['Home', 'EDA', 'Prediction']
+selection = st.sidebar.selectbox('Select a page', options)
+
+# Add the necessary code to display the appropriate page based on the user's selection
+if selection == 'Home':
+   home()
+elif selection == 'EDA':
+   eda()
+else:
+   prediction()
